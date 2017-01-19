38276d5188e1289794c7461afb40b763b22e5848
Jirat K <saakyz@gmail.com>
2017-01-19 11:11:19 +0700

    Initial Project

---
+++ src/test/java/com/n3tr/SpringBootGsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.n3tr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootGsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
